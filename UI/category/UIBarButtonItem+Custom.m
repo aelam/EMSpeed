5c99b8805566ac4f4b2716510c8fa9eecff939f3
Weigang Chen <8337897@qq.com>
2015-05-27 13:51:15 +0800

    update UIBarButtonItem

--- UI/category/UIBarButtonItem+Custom.m
+++ UI/category/UIBarButtonItem+Custom.m
@@ -1,73 +1,18 @@
+////
+////  UIBarButtonItem+EM.m
+////  EMStock
+////
+////  Created by xoHome on 14-11-4.
+////  Copyright (c) 2014年 flora. All rights reserved.
+////
 //
-//  UIBarButtonItem+EM.m
-//  EMStock
+//#import "EMBarButtonItem.h"
+//#import "MSCore.h"
 //
-//  Created by xoHome on 14-11-4.
-//  Copyright (c) 2014年 flora. All rights reserved.
+//#pragma clang diagnostic push
+//#pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
 //
-
-#import "UIBarButtonItem+Custom.h"
-#import "MSCore.h"
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
-
-@implementation MSBarButtonItem
-
-- (instancetype)initWithImage:(UIImage *)image
-                    tintColor:(UIColor *)tintColor
-                        style:(UIBarButtonItemStyle)style
-                       target:(id)target
-                       action:(SEL)action
-{
-    self = [super initWithImage:image style:style target:target action:action];
-    if (self) {
-        self.tintColor = tintColor;//RGB(0x46, 0x90, 0xef);
-        
-    }
-    return self;
-}
-
-- (instancetype)initWithTitle:(NSString *)title
-                    tintColor:(UIColor *)tintColor
-                        style:(UIBarButtonItemStyle)style
-                       target:(id)target
-                       action:(SEL)action
-{
-    self = [super initWithTitle:title style:style target:target action:action];
-    if (self) {
-        self.tintColor = tintColor;//RGB(0x46, 0x90, 0xef);
-    }
-    return self;
-}
-
-@end
-
-
-@implementation UIBarButtonItem(Custom)
-
-+ (NSArray *)ms_rightItemsWithImage1:(UIImage *)image1 target1:(id)target1 action1:(SEL)action1
-                           image2:(UIImage *)image2 target2:(id)target2 action2:(SEL)action2
-{
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    [button setImage:image1 forState:UIControlStateNormal];
-    button.imageEdgeInsets = UIEdgeInsetsMake(5, 0, 0, 0);
-    button.contentVerticalAlignment = UIControlContentVerticalAlignmentTop;
-    
-    CGSize itemSize = CGSizeMake(28, MSNavigationBarHeight()-8);
-    button.frame = CGRectMake(0, 0, itemSize.width, itemSize.height);
-    [button addTarget:target1 action:action1 forControlEvents:UIControlEventTouchUpInside];
-    UIBarButtonItem *item1 = [[UIBarButtonItem alloc] initWithCustomView:button];
-    
-    UIBarButtonItem *item2 =[[MSBarButtonItem alloc] initWithImage:image2
-                                                             style:UIBarButtonItemStylePlain
-                                                            target:target2 action:action2];
-    button.contentHorizontalAlignment = UIControlContentHorizontalAlignmentRight;
-    
-    return [NSArray arrayWithObjects:item2,item1,nil];
-}
-
-@end
-
-
-
+//
+//
+//
+//
