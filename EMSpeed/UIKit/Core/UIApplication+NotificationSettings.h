cee9b30686e84a28abcba6ba1feb19dbf1728d35
Ryan Wang <wanglun02@gmail.com>
2015-12-18 17:29:39 +0800

    UIApplication+NotificationSettings 在extension里面不能使用

--- EMSpeed/UIKit/Core/UIApplication+NotificationSettings.h
+++ EMSpeed/UIKit/Core/UIApplication+NotificationSettings.h
@@ -10,6 +10,6 @@
 
 @interface UIApplication (NotificationSettings)
 
-- (NSUInteger)EMNotificationSettingTypes;
+- (NSUInteger)EMNotificationSettingTypes NS_EXTENSION_UNAVAILABLE_IOS("Use view controller based solutions where appropriate instead.");
 
 @end
