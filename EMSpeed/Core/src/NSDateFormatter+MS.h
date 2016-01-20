4729f88c2fe4bbe33b671e48953237531e66d98c
Flora Deng <dengyanjun@emoney.cn>
2016-01-20 15:53:47 +0800

    增加NSDateFormatter 方法【"04-25 13:39"】

--- EMSpeed/Core/src/NSDateFormatter+MS.h
+++ EMSpeed/Core/src/NSDateFormatter+MS.h
@@ -16,6 +16,8 @@
 
 + (instancetype)ms_shortDateFormatter; // "2015-04-25"
 
++ (instancetype)ms_shortDateTimeFormatter; // "04-25 13:39"
+
 + (instancetype)ms_timeFormatter; // "13:39:09"
 
 + (instancetype)ms_hourMinuteFormatter; // "13:39"
