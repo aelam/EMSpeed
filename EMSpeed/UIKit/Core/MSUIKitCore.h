5ae5172cf54280fc72d04acb725703bf40eac2ec
Ryan Wang <wanglun02@gmail.com>
2016-03-01 16:02:31 +0800

    add UIView+RoundCorner

--- EMSpeed/UIKit/Core/MSUIKitCore.h
+++ EMSpeed/UIKit/Core/MSUIKitCore.h
@@ -14,16 +14,21 @@
 #import <EMSpeed/MSCoreMetrics.h>
 #import <EMSpeed/MSUIKitCoreFunction.h>
 #import <EMSpeed/MSCoreSDKAvailability.h>
-#import <EMSpeed/UIView+AutoLayout.h>
-#import <EMSpeed/UIView-Geometry.h>
 
 #import <EMSpeed/MSUUID.h>
 #import <EMSpeed/UIDevice+IdentifierAddition.h>
 
 #import <EMSpeed/MSViewController.h>
 
+/* UIApplication */
 #import <EMSpeed/UIApplication+NotificationSettings.h>
 #import <EMSpeed/UIApplication+AppVersion.h>
+
+/* UIView */
+#import <EMSpeed/UIView+AutoLayout.h>
+#import <EMSpeed/UIView-Geometry.h>
+#import <EMSpeed/UIView+RoundCorner.h>
+
 
 /* UITableView  */
 #import <EMSpeed/UITableView+CellAction.h>      // will be deprecated later
@@ -31,6 +36,5 @@
 
 /* UINavigationItem  */
 #import <EMSpeed/UINavigationItem+EMBarButtonItem.h> // will be deprecated later
-
 
 #endif /* MSUIKITCORE_H_ */
