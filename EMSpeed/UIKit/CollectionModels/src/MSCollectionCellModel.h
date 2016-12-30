9890a89824f47a7bf6d7c726a6a60a486bd6b82a
flora <dengyanjun@emoney.cn>
2016-12-30 10:15:42 +0800

    优化代码

--- EMSpeed/UIKit/CollectionModels/src/MSCollectionCellModel.h
+++ EMSpeed/UIKit/CollectionModels/src/MSCollectionCellModel.h
@@ -24,7 +24,7 @@
 /**
  *  cellmodel对应的cell的类,请使用cellClass属性设置
  */
-@property (nonatomic, strong) Class Class; __deprecated;
+@property (nonatomic, strong) Class Class; //__deprecated;
 
 /**
  *  cellmodel对应的cell的类，兼容swift
@@ -48,6 +48,16 @@
 @end
 
 @interface MSCollectionCellModel : NSObject <MSCollectionCellModel>
+
+@property (nonatomic, assign) CGSize layoutSize;
+
+@property (nonatomic, strong) Class Class; //__deprecated;
+
+@property (nonatomic, strong) Class cellClass;
+
+@property (nonatomic, strong) NSString *reuseIdentify; //#__deprecated;
+
+@property (nonatomic, assign) BOOL isRegisterByClass;
 
 @end
 
--- EMSpeed/UIKit/CollectionModels/src/MSImageCollectionItem.h
+++ EMSpeed/UIKit/CollectionModels/src/MSImageCollectionItem.h
@@ -14,4 +14,14 @@
 @property (nonatomic, strong) NSString *imageURL;
 @property (nonatomic, strong) NSString *title;
 
+@property (nonatomic, assign) CGSize layoutSize;
+
+@property (nonatomic, strong) Class Class; //__deprecated;
+
+@property (nonatomic, strong) Class cellClass;
+
+@property (nonatomic, strong) NSString *reuseIdentify; //#__deprecated;
+
+@property (nonatomic, assign) BOOL isRegisterByClass;
+
 @end
